dde7bb9cabd530dd89b862b86fc2e551ff1fe851
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2022-09-04 19:24:51 +0200

    1 file modified

--- libraries/binding/bind.js
+++ libraries/binding/bind.js
@@ -2,7 +2,6 @@ import DOMHelper from './libraries/binding/helpers/DOMHelper.js';
 
 class Bind {
   init() {
-    // const all = DOMHelper.getAllNodesFromBody();
     const all = document.getElementsByTagName('*');
     console.log('all');
     console.log(all);
