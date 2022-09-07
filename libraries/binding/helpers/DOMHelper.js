a3afbc5ad5a77815aee301f65412b74c71d838a9
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2022-09-07 23:21:38 +0200

    clean

--- libraries/binding/helpers/DOMHelper.js
+++ libraries/binding/helpers/DOMHelper.js
@@ -1,13 +1,7 @@
 class DOMHelper {
-    static getAllNodesFrom(element) {
-        return document.querySelectorAll(element);
-    }
-    static getAllNodesFromBody() {
-        return document.getElementsByTagName('body');
-    }
-    static getAllNodesInDOM() {
-        return document.getElementsByTagName('*');
-    }
+  static getAllNodesInDOM() {
+    return document.getElementsByTagName('*');
+  }
 }
 
 module.exports = DOMHelper;
