436bc8010b77a550d8876976b8efdd908c9fc6d7
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2022-09-08 22:19:09 +0200

    1 file modified

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ console.log('nodesInfo');
 console.log(nodesInfo); 
 const state = binder.buildState(nodesInfo);
 console.log(state);
-// ORA PER OGNI NODO (IN NODES) applico un evento 
-// per bindare state con l'elemento sulla DOM
+/* ORA PER OGNI NODO (IN NODES) applico un evento 
+ per bindare state con l'elemento sulla DOM */
 binder.associateStateAndNodeUsingEvents(nodesInfo, state); 
+/* • Continuare ocn gli altri tipi di elemementi. 
+   • faare in modo che si possa configurare manualmente
+     quale campo di state sta in binding con quale nodo 
+     (avente una certa id). 
+     ES: Mi serve per fare in modo che un paragrafo in binding
+     con la stessa variabile già in binding con una input possa
+     aggiornarsi appena cambia la variabile. 
+*/
+
+
 
 // setInterval(() => {
 //   console.log('state')
-//   console.log(state)
+//   console.log(state) 
 // }, 5000)
