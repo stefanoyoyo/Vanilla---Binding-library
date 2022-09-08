93b164443273b33a011adf0c8557b600f078e52d
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2022-09-08 22:07:28 +0200

    1 file modified

--- libraries/binding/bind.js
+++ libraries/binding/bind.js
@@ -38,12 +38,25 @@ class Bind {
 
   }
 
-  /**Method binding  anode with its assocoated state field */
+  /**Method binding a node with its assocoated state field */
   bindNodeWithStateField(nodeInfo, state) {
     if (nodeInfo == null) return; 
     if (state == null) return; 
+    switch(nodeInfo.type.toLowerCase()) {
+      case 'input': 
+        this.bindInputWithStateField(nodeInfo, state);
+        break;
+    }
 
     return;
+  }
+
+  /**Method binding a node of type INPUT
+   * with its assocoated state field */
+  bindInputWithStateField(nodeInfo, state) {
+    if (nodeInfo == null) return; 
+    if (state == null) return; 
+    
   }
 
   // #endregion
